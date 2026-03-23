fa6ea23b4b322a05257e5ddac607f800bd037193
Pedro-lsLobo <148923293+Pedro-lsLobo@users.noreply.github.com>
2026-03-23 16:06:44 -0300

    Update client.py

--- client.py
+++ client.py
@@ -2,8 +2,37 @@ from socket  import *
 from constCS import * #-
 
 s = socket(AF_INET, SOCK_STREAM)
-s.connect((HOST, PORT)) # connect to server (block until accepted)
-s.send(str.encode('Hello, world'))  # send some data
-data = s.recv(1024)     # receive the response
-print (bytes.decode(data))            # print the result
-s.close()               # close the connection
+s.connect((HOST, PORT))
+s.send(str.encode('Hello, world'))
+data = s.recv(1024)
+print (bytes.decode(data))
+s.close()
+[ec2-user@ip-172-31-67-45 ~]$ cat new_client.py 
+from socket import *
+from constCS import *
+import time
+
+s = socket(AF_INET, SOCK_STREAM)
+s.connect((HOST, PORT))
+
+print("Calculadora RPC")
+print("Comandos:")
+print("ADD a b | SUB a b | MUL a b | DIV a b | POW a b | SQRT a")
+
+while True:
+    msg = input("\nDigite: ")
+    if msg.lower() == "sair":
+        break
+
+    inicio = time.time()
+
+    s.send(msg.encode())
+    data = s.recv(4096)
+
+    fim = time.time()
+    tempo_total = fim - inicio
+
+    print("Resultado:", data.decode())
+    print(f"Tempo: {tempo_total:.6f}s")
+
+s.close()
